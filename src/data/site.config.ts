9e54b705e717bc425e6750b2aab17d2c87e6f04a
ivanpalachuk <ivan.palachuk@outlook.com>
2024-03-28 14:57:41 -0300

    first Commit

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,11 +9,11 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	author: 'ViCo', // Site author
+	title: 'ViCo Blog', // Site title.
+	description: 'A blog for ViCo Virtual Concierge about the latest technologies and trends', // Description to display in the meta tags
+	lang: 'en-EU',
+	ogLocale: 'en_EU',
+	shareMessage: 'Hey! check this awesome post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
